c15f6bc37d0cbc381b7a267e658ca237a57e50b3
zekariasberhe2022 <zekariasberhe2022@gmail.com>
2023-01-18 18:34:33 +0300

    Creating Common Components

---
+++ src/components/UserDetail.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UserDetail = ({ UserImage, UserName, LastChat }) => {
+  return (
+    <>
+      <div className="w-full flex gap-6 items-center">
+        <img
+          src={UserImage}
+          alt="User Profile"
+          className="h-10 w-10 object-cover rounded-full bg-white"
+        />
+
+        <div className="flex flex-col items-start">
+          <p className="text-sm font-semibold text-white">{UserName}</p>
+          <p className="text-[12px] opacity-80 text-white font-light">
+            {LastChat}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserDetail;
